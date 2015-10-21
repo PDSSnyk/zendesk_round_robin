74d788c36dcc4456d75db3977d1bcd016f0ae390
Ahmad Hassan <er.ahmad.hassan@gmail.com>
2015-10-21 18:23:55 +0530

    added ticketProvider

--- src/main/java/RoundRobin.java
+++ src/main/java/RoundRobin.java
@@ -1,5 +1,10 @@
 import com.zendesk.BasicAuthenticationHandler;
 import com.zendesk.ZendeskClient;
+import com.zendesk.logger.Logger;
+import com.zendesk.model.internal.TicketWrapper;
+import com.zendesk.provider.TicketProvider;
+import com.zendesk.service.ErrorResponse;
+import com.zendesk.service.ZendeskCallback;
 
 /**
  * Created by ahmad.hassan on 21/10/15.
@@ -14,6 +19,20 @@ public class RoundRobin {
         // TicketProvider ticketProvider = new TicketProvider(client.getAdapter()); ticketProvider.getTicketById(1234L, new ZendeskCallback() { @Override public void onSuccess(TicketWrapper ticketWrapper) {
         // We have the ticket } @Override public void onError(ErrorResponse errorResponse) { Logger.e("Sample", errorResponse); } });
         // } }
+
+        TicketProvider ticketProvider = new TicketProvider(client.getAdapter());
+
+        ticketProvider.getTicketById(1, new ZendeskCallback<TicketWrapper>() {
+            @Override
+            public void onSuccess(TicketWrapper ticketWrapper) {
+                System.out.println(ticketWrapper);
+            }
+
+            @Override
+            public void onError(ErrorResponse errorResponse) {
+                Logger.e("Sample", errorResponse);
+            }
+        });
     }
 
 }
